fa9629457245099ba15c348a7f11e31c036ef849
Vedika Sontakke <vhs@umich.edu>
2026-02-22 14:50:47 -0800

    Delete CreateViewScript.sql

--- CreateViewScript.sql
+++
@@ -1,5 +0,0 @@
-create view vw_ChurnData as
-	select *from prod_Churn where customer_status in ('Churned', 'Stayed')
-
-create view vw_JoinData as
-	select *from prod_Churn where customer_status in ('Joined')
